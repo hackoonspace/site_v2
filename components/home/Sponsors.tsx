79b5e49f6d921962742d8a31422fd759703b505d
Infinitemarcus <marcus.natrielli@outlook.com>
2022-11-20 17:01:27 -0300

    Fixing Sponsors declaration ?

--- components/home/Sponsors.tsx
+++ components/home/Sponsors.tsx
@@ -1,11 +1,11 @@
-function Patrocinio() {
+function Sponsors() {
 	return ( 
 		<div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
 			<div className="spaceBorder">
 				<div className="patrocinadores">
 					<h1>Patrocinador</h1>
 					<a href="http://mindlabs.com.br/" target="_blank" rel="noreferrer" title="Site da MindLabs">
-						<img className="patrocinadorImg interactiveImg" alt="Logo da empresa MindLabs" src="/mind.svg"/>
+						<img className="patrocinadorImg interactiveImg" alt="Logo da empresa MindLabs" src="/mind.svg"></img>
 					</a>
 				</div>
 			</div>
@@ -13,4 +13,4 @@ function Patrocinio() {
 	);
 }
 
-export default Patrocinio;
+export default Sponsors;
