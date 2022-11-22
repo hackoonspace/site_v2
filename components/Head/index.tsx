91992785c7d8790a74aceb1816581ef324f19b25
Infinitemarcus <marcus.natrielli@outlook.com>
2022-11-22 14:24:45 -0300

    updating card thumbnail

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -9,7 +9,7 @@ export default function Head() {
             <meta name="title" property="og:title" content='HackoonSpace'/>
             <meta property="og:type" content="website"/>
             <meta property="og:description" name="description" content='Site do HackoonSpace, o primeiro hackerspace e comunidade hacker da UFSCar Sorocaba'/>
-            <meta name="image" property="og:image" content="/HackoonLogo.png"/>
+            <meta name="image" property="og:image" content="/hacka_banner.png"/>
 
             <meta name="twitter:card" content="summary"/>
             <meta name="twitter:site" content="@HackoonSpace"/>
@@ -18,7 +18,7 @@ export default function Head() {
             <meta property="twitter:url" content="https://hackoonspace.com"/>
             <meta name="twitter:title" content='HackoonSpace'/>
             <meta name="twitter:description" content='Site da HackoonWeek, o primeiro hackerspace e comunidade hacker da UFSCar Sorocaba'/>
-            <meta name="twitter:image:src" content="/HackoonLogo.png"/>
+            <meta name="twitter:image:src" content="/hacka_banner.png"/>
 
             <meta name="author" content="HackoonSpace"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
