210a25ccf3203f0056ab9ad1075293d2058ac1c7
Infinitemarcus <marcus.natrielli@outlook.com>
2022-11-22 14:19:14 -0300

    updating head

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -5,20 +5,20 @@ export default function Head() {
         <NextHead>
             <link rel="icon" href="/favicon.ico" />
 
-            <title>HackoonWeek</title>
-            <meta name="title" property="og:title" content='HackoonWeek'/>
+            <title>HackoonSpace</title>
+            <meta name="title" property="og:title" content='HackoonSpace'/>
             <meta property="og:type" content="website"/>
-            <meta property="og:description" name="description" content='Site da HackoonWeek, o evento anual realizado pela comunidade HackoonSpace'/>
-            <meta name="image" property="og:image" content="/hackoonweek2021_logo.png"/>
+            <meta property="og:description" name="description" content='Site do HackoonSpace, o primeiro hackerspace e comunidade hacker da UFSCar Sorocaba'/>
+            <meta name="image" property="og:image" content="/HackoonLogo.png"/>
 
             <meta name="twitter:card" content="summary"/>
             <meta name="twitter:site" content="@HackoonSpace"/>
             <meta name="twitter:creator" content="@HackoonSpace"/>
-            <meta property="twitter:domain" content="hackoonweek.com"/>
-            <meta property="twitter:url" content="https://hackoonweek.com"/>
-            <meta name="twitter:title" content='HackoonWeek'/>
-            <meta name="twitter:description" content='Site da HackoonWeek, o evento anual realizado pela comunidade HackoonSpace'/>
-            <meta name="twitter:image:src" content="/hackoonweek2021_logo.png"/>
+            <meta property="twitter:domain" content="hackoonspace.com"/>
+            <meta property="twitter:url" content="https://hackoonspace.com"/>
+            <meta name="twitter:title" content='HackoonSpace'/>
+            <meta name="twitter:description" content='Site da HackoonWeek, o primeiro hackerspace e comunidade hacker da UFSCar Sorocaba'/>
+            <meta name="twitter:image:src" content="/HackoonLogo.png"/>
 
             <meta name="author" content="HackoonSpace"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
