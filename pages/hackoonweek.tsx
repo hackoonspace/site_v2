0135e57c03a9f87177d967e451ecf46d50a9b11c
fernando <fernandof.abromovick@hotmail.com>
2023-11-22 21:44:21 -0300

    mais mudanças para 2023

--- pages/hackoonweek.tsx
+++ pages/hackoonweek.tsx
@@ -15,7 +15,7 @@ export default function HackoonWeek() {
                             A HackoonWeek é um evento anual sobre cultura hacker, programação e o mundo da computação no geral. Ele é realizado pelo hackerspace <a className='link' href="https://hackoonspace.com" rel="noreferrer" target="_blank">HackoonSpace</a>, localizado na Universidade Federal de São Carlos, em Sorocaba.
                         </p>,
                         <p key='panel-2' className='paragraph'>
-                            A edição de 2022 será entre os dias 12/12 e 16/12 e terá várias atividades diferentes, com o intuito de agregar conhecimento e apresentar nosso trabalho para vocês. Também teremos a participação especial de alguns convidados.
+                            A edição de 2023 será entre os dias 04/12 e 08/12 e terá várias atividades diferentes, com o intuito de agregar conhecimento e apresentar nosso trabalho para vocês. Também teremos a participação especial de alguns convidados.
                         </p>,
                         <p key='panel-3' className='paragraph'>
                             Para participar, faça o cadastro no formulário do botão abaixo. Se inscreva nas atividades que você deseja e valide sua presença durante elas ao vivo no nosso <a className='link' href='https://youtube.com/@hackoonspace' rel='noreferrer' target='_blank'>canal do Youtube</a>. Ao final do evento, você poderá obter um certificado de participação.
