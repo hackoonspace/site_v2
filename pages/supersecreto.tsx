0e345e5bef3a293e7c24a4dba518520816897dda
kyleflick124 <fernandof.abromovick@hotmail.com>
2025-03-12 18:39:37 -0300

    cabo

--- pages/supersecreto.tsx
+++ pages/supersecreto.tsx
@@ -13,9 +13,11 @@ export default function SuperSecreto() {
                     fontSize: 30
                 }}
             >
-                <p>Você conseguiu!</p>
-                <p style={{color: 'yellow'}}>A senha é {'"hackoonspace321"'}</p>
-                <p>Mostre essa tela para um de nossos membros.</p>
+                <p>Fez bem em encontrar essa página.</p>
+                <p>Consegui roubar os arquivos do ladrão. Quem diria que teríamos o roubo do roubo?</p>
+                <p style={{color: 'yellow'}}>no fim, tinha só umas coisas nada a ver. Mas eu destruí tudo, só o que sobrou foi esse presente para você: {'"HW2024{but_R1DDL3R_has_the_files}"'}</p>
+                <p>Já encontrou o verdadeiro ladrão? Já achou todas as flags? O tempo está passando.</p>
+                <p>- R</p>
             </div>
         </>
     );
